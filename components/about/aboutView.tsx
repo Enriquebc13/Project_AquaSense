cab9cb604f31c88bf4a7d7cf0b1505b44e566d6f
chenaoh <cdhenao@gmail.com>
2025-02-13 23:11:53 -0600

    Actualizacion de estructura de carpetas

--- components/about/aboutView.tsx
+++ components/about/aboutView.tsx
@@ -1,9 +1,47 @@
-import { Text } from "react-native";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 
-export default function AboutView(){
-    return ( 
-        <Text style={{textAlign:"center",
-            color:"black",
-        }}>Holaaaa</Text>
+export default function AboutView() {
+    return (
+        <ImageBackground  style={styles.fondo}
+        source={require("../../assets/images/fondo.jpg")}>
+            <Text style={styles.title}>Acerca de</Text>
+            <View style={styles.container}>
+                <Text style={styles.text}>
+                Esta App Fue creada por estudiantes {"\n"}
+                de la Universidad Tecnologica de Izucar de Matamoros {"\n"}
+                </Text>
+         
+            </View>
+        </ImageBackground>
+
     )
 }
+
+const styles = StyleSheet.create({
+    fondo: {
+         height:"100%",
+         width:"100%",
+         alignItems:"center",
+    },
+    title: {
+        fontSize: 40,
+        color: "black",
+        fontWeight: "bold",
+        textAlign: "center",
+    },
+    container: {
+        marginTop:40,
+        width:350,
+        height:400,
+        borderRadius:10,
+        borderWidth:3,
+        borderColor:"black",
+        backgroundColor:"rgba(0, 0, 0, 0.4)",
+        padding:10,
+    },
+    text: {
+        color:"white",
+        fontSize:16,
+        textAlign:"center",
+    }
+})
